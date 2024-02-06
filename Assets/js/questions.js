5a899796f217a6c61556907849d45902a749e5fc
lewiscornelius <lewiscornelius7@gmail.com>
2024-02-05 19:37:25 -0500

    finished assignment

---
+++ Assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commnly used date types DO NOT include:",
+        choices:["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "Which of the following is NOT a programming language:",
+        choices:["Python", "Javascript", "SQL", "HTML"],
+        answer: "HTML",
+    },
+    {
+        title: "In object-oriented programming, what does the term 'inheritance' refer to?",
+        choices:["The process of creating multiple instances of a class.", "The ability of a class to take on the properties and methods of another class.", "The process of converting source code into machine code.", "The mechanism for restricting access to certain components of a class."],
+        answer: "The ability of a class to take on the properties and methods of another class.",
+    },
+    {
+        title: "Which term in programming refers to the ability to take on the properties and methods of another class?",
+        choices:["Polymorphism", "Encapsulation", "Inheritance", "Abstraction"],
+        answer: "Inheritance",
+    },
+    {
+        title: "What is the term for the process of converting source code into machine code that can be executed by a computer's CPU?",
+        choices:["Compilation", "Interpretation", "Execution", "Debugging"],
+        answer: "Compilation",
+    },
+]
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Full-Stack-Study-Guide
+
+## User Story
+
+```
+As A coder 
+I WANT to be able to take a timed quiz on Full-Stack  development 
+SO THAT I can test my knowledge and reinforce what I have learned.
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a timed quiz on Full Stack Development
+WHEN I click start
+THEN the timer starts counting down
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer  a question incorrectly
+THEN time is taken away from the clock
+When all questions are answered or the timer reaches zero
+THEN the game is over 
+WHEN the game is over
+THEN I am presented with my final score and save with my initials
+```
